b6e118cb6eb408ab63bac7f797140eb27d7606ea
Davor <nikoloski.davorr@gmail.com>
2025-05-17 21:25:16 +0200

    Improved BeAware and added dynamic card and modal rendering using redux

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,11 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { useSelector } from 'react-redux';
 import Tab2 from './pages/Tab2';
-
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-
 import './app.css';
-
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 import '@ionic/react/css/normalize.css';
@@ -29,85 +27,100 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 import './theme/variables.css';
-
 import Wrapper from './components/Wrapper';
 import UserInfo from './components/User/UserInfo';
-import BeAware from './components/BeAware';
+import BeAware from './components/BeAware/BeAware';
 import Home from './components/Home/Home';
+
+// Define RootState type
+interface RootState {
+  userAuth: {
+    isAuthenticated: boolean;
+  };
+}
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
+const App: React.FC = () => {
+  // Get authentication status from Redux store
+  const isAuthenticated = useSelector((state: RootState) => state.userAuth.isAuthenticated);
 
-          {/* Tabs */}
-          <Route
-            exact
-            path="/Home"
-            render={() => (
-              <Wrapper>
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            {/* Tabs */}
+            <Route
+              exact
+              path="/Home"
+              render={() => (
+                <Wrapper>
                   <Home />
-              </Wrapper>
-            )}
-          />
-          <Route
-            exact
-            path="/Be-Aware"
-            render={() => (
-              <Wrapper>
+                </Wrapper>
+              )}
+            />
+            <Route
+              exact
+              path="/Be-Aware"
+              render={() => (
+                <Wrapper>
                   <BeAware />
-              </Wrapper>
-            )}
-          />
-          <Route
-            exact
-            path="/Chat"
-            render={() => (
-              <Wrapper>
-                <Tab2 />
-              </Wrapper>
-            )}
-          />
-          <Route
-            exact
-            path="/Profile"
-            render={() => (
-              <Wrapper>
+                </Wrapper>
+              )}
+            />
+            <Route
+              exact
+              path="/Chat"
+              render={() => (
+                <Wrapper>
+                  <Tab2 />
+                </Wrapper>
+              )}
+            />
+            <Route
+              exact
+              path="/Profile"
+              render={() => (
+                <Wrapper>
                   <UserInfo />
-              </Wrapper>
-            )}
-          />
-
-          {/* Auth - now wrapped in Wrapper */}
-          <Route
-            exact
-            path="/login"
-            render={() => (
-              <Wrapper>
-                <Login />
-              </Wrapper>
-            )}
-          />
-          <Route
-            exact
-            path="/register"
-            render={() => (
-              <Wrapper>
-                <Register />
-              </Wrapper>
-            )}
-          />
-
-          {/* Redirect root to tab1 */}
-          <Route exact path="/" render={() => <Redirect to="/Home" />} />
-
-        </IonRouterOutlet>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+                </Wrapper>
+              )}
+            />
+            {/* Auth routes with redirect when authenticated */}
+            <Route
+              exact
+              path="/login"
+              render={() => 
+                isAuthenticated ? (
+                  <Redirect to="/Home" />
+                ) : (
+                  <Wrapper>
+                    <Login />
+                  </Wrapper>
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={() => 
+                isAuthenticated ? (
+                  <Redirect to="/Home" />
+                ) : (
+                  <Wrapper>
+                    <Register />
+                  </Wrapper>
+                )
+              }
+            />
+            {/* Redirect root to tab1 */}
+            <Route exact path="/" render={() => <Redirect to="/Home" />} />
+          </IonRouterOutlet>
+        </IonTabs>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
